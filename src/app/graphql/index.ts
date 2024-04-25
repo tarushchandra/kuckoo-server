1d66c47e62ade6b37bcb702233581ddf649754c8
Tarush Chandra <tarushchandra6@gmail.com>
2024-04-25 15:33:37 +0530

    added tweet model and graphql setup

--- src/app/graphql/index.ts
+++ src/app/graphql/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
-import { User } from "./user";
 import { JwtUser } from "../services/user";
+import { User } from "./user";
+import { Tweet } from "./tweet";
 
 export interface GraphqlContext {
   user?: JwtUser;
@@ -10,23 +11,29 @@ async function createApolloGraphQLServer() {
   const gqlServer = new ApolloServer<GraphqlContext>({
     typeDefs: `
         ${User.typeDefs}
+        ${Tweet.typeDefs}
 
         type Query {
             ${User.queries}
+            ${Tweet.queries}
         }
 
         type Mutation {
             ${User.mutations}
+            ${Tweet.mutations}
         }
     `,
     resolvers: {
       Query: {
         ...User.resolvers.queries,
+        ...Tweet.resolvers.queries,
       },
       Mutation: {
         ...User.resolvers.mutations,
+        ...Tweet.resolvers.mutations,
       },
       ...User.resolvers.extraResolvers,
+      ...Tweet.resolvers.extraResolvers,
     },
   });
 
