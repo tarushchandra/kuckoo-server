87cfe52073de5c6bc827a67a179f59b19d997ccb
Tarush Chandra <tarushchandra6@gmail.com>
2025-09-08 15:31:40 +0530

    feat: added frontend server url in env variable

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
+    FRONTEND_SERVER_URL: string;
     PORT?: string;
     DATABASE_URL: string;
     DIRECT_URL: string;
